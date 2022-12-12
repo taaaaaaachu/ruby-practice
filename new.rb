a9e4ff93a8add9b5ac949886f7693346bd37c72a
Tachu <stitch_ditto_0j@icloud.com>
2022-12-12 15:52:52 +0900

    a

---
+++ new.rb
@@ -0,0 +1,13 @@
+class Car
+    def run(distance)
+        puts "車で#{distance}キロ走ります。"
+    end
+
+    def turn(direction)
+        puts "#{direction}に曲がります。"
+    end
+end
+
+car = Car.new
+car.run(5)
+car.turn("right")
