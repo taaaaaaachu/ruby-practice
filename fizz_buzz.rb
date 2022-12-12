bbbb798b0cc9ba151b34e54e72b16c111cfdd3c6
Tachu <stitch_ditto_0j@icloud.com>
2022-12-12 15:45:12 +0900

    fix

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,10 +1,10 @@
 def fizz_buzz(number)
-    if number % 3 == 0
+    if number % 15 == 0
+        puts "FizzBuzz"
+    elsif number % 3 == 0
         puts "Fizz"
     elsif number % 5 == 0
         puts "Buzz"
-    elsif number % 15 == 0
-        puts "FizzBuzz"
     else
         puts number
     end
